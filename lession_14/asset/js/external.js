0d6af116778dd7de2b88167e0e26d6f4eff8b3ae
duy366110 <duy366110@gmail.com>
2023-07-15 15:45:57 +0700

    #Commit 15/07/2023: Config lession 14.

---
+++ lession_14/asset/js/external.js
@@ -0,0 +1,35 @@
+window.onload = function(event) {
+
+    var team = 10;
+    var _team = 10;
+    var $team = 10;
+
+    // var 0team = 10;
+    // var tea&m = 10;
+    // var this = 10;
+
+    /* BUILT - IN - ALERT */
+    alert('Hello world');
+
+    /* BUILT - IN - CONSOLE */
+    console.log('Hello word');
+
+    /* BUILT - IN - CONFIRM */
+    var confirmVal = confirm('Text');
+    console.log(confirmVal);
+
+    /* BUILT - IN - ALERT */
+    let promptVal = prompt('Age');
+    console.log(promptVal);
+
+    /* BUILT - IN - SETTIME OUT */
+    setTimeout(() => {
+        console.log('Hello setTimeout');
+    }, 1500)
+
+    /* BUILT - IN - SETINTERVAL */
+    setInterval(() => {
+        console.log('Hello setInterval');
+    }, 2500)
+
+}
---
+++ lession_14/asset/js/operator.js
@@ -0,0 +1,134 @@
+window.onload = function(event) {
+    /**
+     * TOÁN TỬ SỐ HỌC
+      +    -> cộng
+      -    -> trừ
+      *    -> nhân
+      **   -> luỹ thừa
+      /    -> chia lấy nguyên
+      %    -> chia lấy dư
+      ++   -> tăng 1 giá trị
+      --   -> giảm 1 giá trị
+    */
+
+      var a = 1;
+      var b = 2;
+
+      var cong = a + b;
+      console.log("Cộng: " + cong);
+
+      var tru = a - b;
+      console.log('Trừ: ' + tru);
+
+      var nhan = a * b;
+      console.log("Nhân: " + nhan);
+
+      var luythua = b**2;
+      console.log("Luỹ thừa: " + luythua);
+
+      var chia = 4 / 2;
+      console.log("Chia lấy nguyên: " + chia);
+
+      var chiadu = 4 % 2;
+      console.log("Chia lấy dư: " + chiadu);
+
+      // Toán tử tiền tố và toán tử hậu tố
+      var a = 1;
+
+      // step 1: cộng 1 cho a, a = a + 1, a = 2
+      // step 2: trả về a = 7 và in ra màng hình
+      ++a;
+      console.log(a);
+
+
+      // step 1: coppy a ra một biết gọi là a' = 6
+      // step 2: thực hiện cộng 1 cho a, a = a + 1, a = 2
+      // step 3: trả về a' coppy in ra màng hình
+      var a = 1;
+      var team  = a++;
+      console.log("A' coppy: " + team);
+      console.log("A nguyên mẫu: " + a);
+
+
+
+    /**
+     * TOÁN TỬ GÁN
+      TOÁN TỬ           VÍ DỤ          KẾT QUẢ
+        =               A = B           A = B
+        +=              A += B          A = A + B
+        -=              A -= B          A = A - B
+        *=              A *= B          A = A * B
+        **=             A **= B         A = A ** B
+        /=              A /= B          A = A / B
+        %=              A %= B          A = A % B
+    */
+
+    var a = 8;
+    
+    // var a = a + 1;
+    a += 1;
+    console.log("Toán tử += :" + a);
+
+    // var a = a - 1;
+    a -= 1;
+    console.log('Toán tử -= :' + a);
+
+    // var a = a * 2;
+    a *= 2;
+    console.log("Toán tử *= :" + a);
+
+    // a = a ** 2;
+    a **= 2;
+    console.log('Toán tử **= :' + a);
+
+    // a = a / 2;
+    a /= 16;
+    console.log('Toán tử /= :' + a);
+
+    // a = a % 2;
+    a %= 2;
+    console.log("Toán tử %= :" + a);
+
+
+
+    /**
+     * TOÁN TỬ NỐI CHUỖI
+    */
+
+    var firstName = 'Nguyễn';
+    var lastName = 'Khải';
+    var fullName = firstName + " " + lastName;
+
+    fullName += ' Test';
+    console.log(fullName);
+
+    /**
+     * TOÁN TỬ SO SÁNH
+      ==        -> bằng
+      ===       -> bằng cả về kiểu dữ liệu
+      <         -> nhỏ
+      >         -> lớn
+      <=        -> nhỏ hơn hoặc bằng
+      >=        -> lớn hơn hoặc bằng
+      !=        -> khác
+      !==       -> khác cả về kiểu dữ liệu
+    */
+
+    var a = 1;
+    var b = 2;
+
+    var c = 1;
+    var d = '1';
+
+    console.log("So sánh bằng: ", a == b);
+    console.log("So sánh khác: ", a != b);
+    console.log("So sánh lớn hơn: ", a > b);
+    console.log("So sánh nhở hơn: ", a < b);
+
+
+    console.log("So sánh bằng so sánh cả kiểu dữ liêu: ", a == c);
+    console.log("So sánh bằng so sánh cả kiểu dữ liêu: ", a === c);
+
+    console.log("So sánh bằng so sánh cả kiểu dữ liêu: ", a == d);
+    console.log("So sánh bằng so sánh cả kiểu dữ liêu: ", a === d);
+}
