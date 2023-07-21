8ba4c7f271a5f1a0f4249117af26d1b4fdbdf953
duy366110 <duy366110@gmail.com>
2023-07-21 22:05:34 +0700

    #Commit 21/07/2023: Config lession 17, 18, 19, 20.

---
+++ lession_17_18_19_20/asset/js/array.js
@@ -0,0 +1,65 @@
+window.onload = function(event) {
+    let arr_01 = [1, 2, 3, 4, 5];
+
+    let arr_02 = new Array(10);
+
+    arr_02[0] = 6;
+    arr_02[1] = 7;
+
+    console.log(arr_01);
+    console.log(arr_02);
+
+
+    // VÒNG LẶP FOR
+    console.log('FOR');
+    for(let i = 0; i < arr_02.length; i++) {
+        console.log(arr_02[i]);
+    }
+
+
+    // VÒNG LẶP WHILE
+    console.log('WHILE');
+    let i = 0;
+    while(i < arr_02.length) {
+        console.log(arr_02[i]);
+        i++;
+    }
+    
+    // VÒNG LẶP FOREACH
+    console.log('FOREACH');
+    arr_02.forEach((elm, index, arr_origin) => {
+        console.log(elm);
+
+    })
+
+    // FOR OF
+    console.log('FOR OF');
+    for(let val of arr_02) {
+        console.log(val);
+    }
+
+    // THÊM PHẦN TỬ VÀO ĐẦU MẢNG
+    console.log('UNSHIFT');
+    arr_02.unshift(8);
+    console.log(arr_02);
+
+    // XOÁ PHẦN TỬ ĐẦU MẢNG
+    console.log('SHIFT');
+    arr_02.shift();
+    console.log(arr_02);
+
+    // THÊM PHẦN TỬ VÀO CUỐI MẢNG
+    console.log('PUSH');
+    arr_02.push(11);
+    console.log(arr_02);
+
+    // XOÁ PHẦN TỬ CUỐI MẢNG
+    console.log('POP');
+    arr_02.pop();
+    console.log(arr_02);
+
+    // XOÁ MẢNG TẠI VỊ TRÍ BẤT KỲ
+
+    arr_01.splice(1, 1, 19);
+    console.log(arr_01);
+}
